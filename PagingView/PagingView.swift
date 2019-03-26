7e8f96013a404fcc453a851a48045731a3fbb893
Mike Glass <mike@breezyhr.com>
2019-03-26 10:53:46 -0400

    Update to Swift 5

--- PagingView/PagingView.swift
+++ PagingView/PagingView.swift
@@ -298,7 +298,7 @@ open class PagingView: UIScrollView {
         var item = indexPath.item
         let sections = forceSections()
         
-        let sectionIndex = sections.index(of: indexPath.section)
+        let sectionIndex = sections.firstIndex(of: indexPath.section)
         
         switch position {
         case .left:
